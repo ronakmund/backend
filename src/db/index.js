0115fb7996af95902fd36ae6f36d44230d053979
Ronak Mund <ronakmund3@gmail.com>
2024-05-13 22:05:04 +0530

    fix(database): update connection message

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ export const connectDb = async () => {
     try{
         console.log("URL> ",`${process.env.MONGODB_URL}/${DB_NAME}`)
         const connectionString = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        console.log("DB connected", connectionString)
+        console.log("DB connected to host: ", connectionString.connection.host);
     }catch(error){
         console.error(error);
         process.exit(1);
